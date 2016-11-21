b3e91ea40170f49d09dfa059ef28d4e0b6ca848d
Timothy Strimple <tim@tstrimple.com>
2016-11-21 12:42:33 -0600

    switch to html5 mode

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+	$locationProvider.html5Mode(true).hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
